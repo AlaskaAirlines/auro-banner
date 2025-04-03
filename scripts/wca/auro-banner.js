02cb6e3f5b294b53b776ad48abecd8d7d586b06b
Joe Yang <joe.yang@alaskaair.com>
2025-04-03 14:40:06 -0700

    perf: add wca script for docs api

---
+++ scripts/wca/auro-banner.js
@@ -0,0 +1,11 @@
+
+import { AuroBanner } from '../../src/auro-banner.js';
+
+/**
+ * The auro-banner element provides users a flexible way to convey a summary of information. The primary elements of a banner include an image, and details.
+ * */
+class AuroBannerWCA extends AuroBanner {}
+
+if (!customElements.get("auro-banner")) {
+  customElements.define("auro-banner", AuroBannerWCA);
+}
